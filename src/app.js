4e10aae67c22afe19cf1a9343ad8385053bc4e25
ybellones <ybellones@outlook.com>
2019-07-25 11:52:59 +0800

    file handling and routing

--- src/app.js
+++ src/app.js
@@ -9,6 +9,26 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', {title:'Home'}));
+//Read accounts.json file
+const accountData = fs.readFileSync(path.join(__dirname,'json','accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+//Read users.json file
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => res.render('index', {title:'Account Summary', accounts}));
+
+//savings route
+app.get('/savings', (req, res) => res.render('account', {account:accounts.savings}));
+
+//checking route
+app.get('/checking', (req, res) => res.render('account', {account: accounts.checking}));
+
+//credit route
+app.get('/credit', (req, res) => res.render('account', {account: accounts.credit}));
+
+//profile route
+app.get('/profile', (req,res) => res.render('profile', {user: users[0]}));
 
 app.listen(3000, () => console.log('My project is running on port 3000!'));
