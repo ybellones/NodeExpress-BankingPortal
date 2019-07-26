d2b7816111b4340bf2009b54d71d80742653d33a
ybellones <ybellones@outlook.com>
2019-07-26 12:44:33 +0800

    handling form data
    practice

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
 //Read accounts.json file
 const accountData = fs.readFileSync(path.join(__dirname,'json','accounts.json'), 'utf8');
@@ -30,5 +31,36 @@ app.get('/credit', (req, res) => res.render('account', {account: accounts.credit
 
 //profile route
 app.get('/profile', (req,res) => res.render('profile', {user: users[0]}));
+
+//transfer route
+app.get('/transfer', (req,res) => res.render('transfer'));
+
+//payment GET route
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+
+//******************************************************************************************************* */
+
+//transfer POST route
+app.post('/transfer', (req, res) => {
+    //deduct balance FROM
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+
+    //ADD balance to other account
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance, 10) + parseInt(req.body.amount,10);
+
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+    res.render('transfer', {message: "Transfer Completed"});
+});
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount,10);
+    
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('/payment', {message: "Payment Successful", account: accounts.credit});
+});
 
 app.listen(3000, () => console.log('My project is running on port 3000!'));
